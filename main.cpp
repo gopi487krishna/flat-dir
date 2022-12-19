e3c1036c9b6387f939af647f66a24410cf6b385d
Gopi Krishna Menon <krishnagopi487.github@outlook.com>
2022-12-19 21:53:52 +0530

    Add code to flat the directory

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <filesystem>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  const std::filesystem::path starting_path{std::filesystem::current_path()};
+  std::filesystem::path output_root{"/home/cooldev/outputs/"}; /* Specify where output folders will reside */
+
+  /* Recursively iterate over directories and start checking */
+  for(const auto& parent_dirs : std::filesystem::directory_iterator(starting_path))
+  {
+      int counter  = 0; /*Keep names unique*/
+
+      if (!parent_dirs.is_directory())
+      {
+          continue;
+      }
+
+      std::string parent_dir_name = parent_dirs.path().filename().string();
+
+      /* Create output dir for this one */
+      auto output_dir_name = output_root/parent_dir_name;
+      std::filesystem::create_directory(output_dir_name);
+
+      for(const auto& entry : std::filesystem::recursive_directory_iterator(parent_dirs))
+      {
+
+          auto filename_str = entry.path().filename().string();
+
+          if ( entry.is_directory())
+          {
+              std::cout <<"[dir]" << entry.path().filename().string() << '\n';
+          }
+          else if (entry.is_regular_file())
+          {
+              std::cout <<"[file]" << entry.path().filename().string() << '\n';
+              /*Copy the files to main folder with sequence*/
+              std::filesystem::copy_file(entry, output_dir_name/(filename_str + '_' + std::to_string(counter++))); 
+         }
+      }
+  }
+  return 0;
+}
